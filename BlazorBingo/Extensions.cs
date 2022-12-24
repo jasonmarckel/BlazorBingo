32e460d31daa4e89468e876cc7f884e7dc5afebb
Jason Marckel <jason@wkfmbp21.lan>
2022-12-24 00:26:37 -0500

    updated selection of stamp class

---
+++ BlazorBingo/Extensions.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorBingo;
+
+public static class Extensions
+{
+    public static string CssName(this string value)
+    {
+        return value.Replace(" ", string.Empty).ToLowerInvariant();
+    }
+}
---
+++ BlazorBingo/Settings.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorBingo;
+
+public class Settings
+{
+    public string PlayerName { get; set; } = string.Empty;
+    public int GamesPlayed { get; set; }
+    public int GamesWon { get; set; }
+}
