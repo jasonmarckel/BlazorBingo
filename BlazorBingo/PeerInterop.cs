86d6681472e4269d725cbdb2aeaecd6ddcef9bbb
Jason Marckel <Jason.Marckel@lubrizol.com>
2023-01-04 02:04:50 -0500

    clean-up

--- BlazorBingo/PeerInterop.cs
+++ BlazorBingo/PeerInterop.cs
@@ -17,19 +17,17 @@ public partial class PeerInterop
 	{
     }
 
-    // [JSMarshalAs<JSType.Any>] object component
-
     [JSImport("host", "PeerJs")]
     internal static partial Task Host([JSMarshalAs<JSType.String>] string id);
 
     [JSImport("connect", "PeerJs")]
-    internal static partial Task Connect([JSMarshalAs<JSType.String>] string id, [JSMarshalAs<JSType.String>] string playerName);
-
-    [JSImport("send", "PeerJs")]
-    internal static partial Task Send([JSMarshalAs<JSType.String>] string id, [JSMarshalAs<JSType.String>] string message);
+    internal static partial Task Connect([JSMarshalAs<JSType.String>] string remoteId, [JSMarshalAs<JSType.String>] string playerName);
 
     [JSImport("broadcast", "PeerJs")]
     internal static partial Task Broadcast([JSMarshalAs<JSType.String>] string message);
+
+    [JSImport("notifyHost", "PeerJs")]
+    internal static partial Task NotifyHost([JSMarshalAs<JSType.String>] string message);
 
     [JSExport]
     internal static void OnConnected(string playerName)
@@ -38,11 +36,18 @@ public partial class PeerInterop
     }
 
     [JSExport]
-    internal static void OnReceive(string json)
+    internal static void OnDisconnected(string playerName)
     {
+        Console.WriteLine(playerName + " disconnected.");
+    }
+
+    [JSExport]
+    internal static void OnDataReceived(string data)
+    {
+        // [JSMarshalAs<JSType.Any>] object component
         //DetectHands detectHands = (DetectHands)component;
         //detectHands.DetectionResult = JsonSerializer.Deserialize<DetectionResult>(json, DetectionResult.SerializeOptions);
-        Console.WriteLine("OnReceive " + json); // detectHands.DetectionResult!.Hands.Count);
+        Console.WriteLine("OnDataReceived " + data);
         //detectHands.StateHasChanged();
     }
 
--- BlazorBingo/wwwroot/script/PeerJsInterop.js
+++ BlazorBingo/wwwroot/script/PeerJsInterop.js
@@ -1,6 +1,7 @@
-﻿
+﻿const peerjs = window;
 
-const peerjs = window;
+// references:
+// https://github.com/pavelsavara/blazor-wasm-hands-pose/blob/main/DetectHandsJsComponent/DetectHands.razor.js
 
 function insertGlobalScript(url) {
     var element = document.createElement('script');
@@ -15,19 +16,19 @@ function insertGlobalScript(url) {
     });
 }
 
-let peerjsExports;
-
-let peer;
-let conn;
+let dotnetExports;
+let hostConnection;
 let conns = [];
+const hostDomain = "net-marckel-blazorbingo-";
 
 export async function host(id) {
 
     const { getAssemblyExports } = await globalThis.getDotnetRuntime(0);
-    peerjsExports = await getAssemblyExports("BlazorBingo.dll");
+    dotnetExports = await getAssemblyExports("BlazorBingo.dll");
 
     await insertGlobalScript('../lib/peerjs/peerjs.min.js');
-    peer = new peerjs.Peer(id);
+
+    var peer = new peerjs.Peer(hostDomain + id);
 
     peer.on('open', function (id) {
         console.log('My Peer Id is: ' + id);
@@ -37,39 +38,11 @@ export async function host(id) {
         conns.push(conn);
         console.log('received connection from ' + conn.peer + ' ' + conn.metadata.playerName);
 
-        peerjsExports.BlazorBingo.PeerInterop.OnConnected(conn.metadata.playerName);
+        dotnetExports.BlazorBingo.PeerInterop.OnConnected(conn.metadata.playerName);
 
         conn.on('data', function (data) {
-//            peerjsExports.PeerInterop.OnReceive(component, data);
             console.log(data);
-        });
-    });
-}
-
-export async function connect(remoteId, playerName) {
-
-    const { getAssemblyExports } = await globalThis.getDotnetRuntime(0);
-    peerjsExports = await getAssemblyExports("BlazorBingo.dll");
-
-    await insertGlobalScript('../lib/peerjs/peerjs.min.js');
-    peer = new peerjs.Peer();
-
-    peer.on('open', function (id) {
-        console.log('My Peer Id is: ' + id);
-        console.log('Remote Id:' + remoteId);
-        console.log('Player Name:' + playerName);
-        conn = peer.connect(remoteId, {
-            reliable: true,
-            metadata: {
-                playerName: playerName
-                }
-        });
-        conn.on("open", () => {
-            console.log('connected');
-            //conn.send("hi from " + id);
-        });
-        conn.on("data", (data) => {
-            console.log(data);
+            dotnetExports.BlazorBingo.PeerInterop.OnDataReceived(data);
         });
     });
 
@@ -78,9 +51,42 @@ export async function connect(remoteId, playerName) {
     });
 }
 
-export async function send(id, message) {
-    console.log('send: ' + message + ' to ' + id);
-    conn.send(message);
+export async function connect(remoteId, playerName) {
+
+    const { getAssemblyExports } = await globalThis.getDotnetRuntime(0);
+    dotnetExports = await getAssemblyExports("BlazorBingo.dll");
+
+    await insertGlobalScript('../lib/peerjs/peerjs.min.js');
+
+    var peer = new peerjs.Peer();
+
+    peer.on('open', function (id) {
+        console.log('My Peer Id is: ' + id);
+        console.log('Remote Id:' + remoteId);
+        console.log('Player Name:' + playerName);
+        hostConnection = peer.connect(hostDomain + remoteId, {
+            reliable: true,
+            metadata: {
+                playerName: playerName
+            }
+        });
+        hostConnection.on("open", () => {
+            console.log('connected');
+        });
+        hostConnection.on("data", (data) => {
+            console.log(data);
+            dotnetExports.BlazorBingo.PeerInterop.OnDataReceived(data);
+        });
+    });
+
+    peer.on('error', function (err) {
+        console.log('error ' + err.type);
+    });
+}
+
+export async function notifyHost(message) {
+    console.log('notifyHost: ' + message);
+    hostConnection.send(message);
 }
 
 export async function broadcast(message) {
@@ -95,45 +101,4 @@ const spinner = document.querySelector('.loading');
 spinner.ontransitionend = () => {
     spinner.style.display = 'none';
 };
-*/
-
-
-/*
-function onResults(component, results) {
-    document.body.classList.add('loaded');
-    canvasCtx.save();
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
-    if (results.multiHandLandmarks && results.multiHandedness) {
-        for (let index = 0; index < results.multiHandLandmarks.length; index++) {
-            const classification = results.multiHandedness[index];
-            const isRightHand = classification.label === 'Right';
-            const landmarks = results.multiHandLandmarks[index];
-            drawingUtils.drawConnectors(canvasCtx, landmarks, mpHands.HAND_CONNECTIONS, { color: isRightHand ? '#00FF00' : '#FF0000' });
-            drawingUtils.drawLandmarks(canvasCtx, landmarks, {
-                color: isRightHand ? '#00FF00' : '#FF0000',
-                fillColor: isRightHand ? '#FF0000' : '#00FF00',
-                radius: (data) => {
-                    return drawingUtils.lerp(data.from.z, -0.15, .1, 10, 1);
-                }
-            });
-        }
-    }
-    canvasCtx.restore();
-    if (results.multiHandedness && results.multiHandLandmarks) {
-        const hands = [];
-        for (let index = 0; index < results.multiHandLandmarks.length; index++) {
-            const classification = results.multiHandedness[index];
-            const landmarks = results.multiHandLandmarks[index];
-            hands.push({
-                ...classification,
-                landmarks
-            })
-        }
-
-        const json = JSON.stringify({ hands });
-        detectHandsExports.DetectHandsJsComponent.DetectHands.Interop.OnResults(component, json);
-    }
-}
-
 */
