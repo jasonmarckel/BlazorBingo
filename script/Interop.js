cb0b94ae329ad736590a21c249e58c4d61d7b140
jasonmarckel <jason@marckel.net>
2023-01-07 20:48:13 +0000

    Deploying to gh-pages from  @ 0e379855da7c7ac5ce13acfef5cdeb834c2fb546 🚀

---
+++ script/Interop.js
@@ -0,0 +1,215 @@
+﻿// https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance
+// https://github.com/mdn/dom-examples/tree/main/web-speech-api/speak-easy-synthesis
+// https://mdn.github.io/dom-examples/web-speech-api/speak-easy-synthesis/
+
+const synth = window.speechSynthesis;
+let voiceSelect;
+let voices = [];
+
+function populateVoiceList() {
+    voices = synth.getVoices().sort(function (a, b) {      
+        const aname = a.name.toUpperCase();
+        const bname = b.name.toUpperCase();
+
+        // sort by language and then by speaker name
+        if (a.lang < b.lang) {
+            return -1;
+        } else if (a.lang > b.lang) {
+            return +1;
+        } else if (aname < bname) {
+            return -1;
+        } else if (aname == bname) {
+            return 0;
+        } else {
+            return +1;
+        }
+    });
+    var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+    voiceSelect.innerHTML = "";
+
+    var preferredLanguage = navigator.language.toUpperCase();
+
+    for (let i = 0; i < voices.length; i++) {
+        // select the first speaker whose language matches the user's preferred language
+        if (selectedIndex == 0 && voices[i].lang.toUpperCase() === preferredLanguage) { selectedIndex = i; }
+        const option = document.createElement("option");
+        option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+        if (voices[i].default) {
+            option.textContent += " -- DEFAULT";
+        }
+
+        option.setAttribute("data-lang", voices[i].lang);
+        option.setAttribute("data-name", voices[i].name);
+        voiceSelect.appendChild(option);
+    }
+    voiceSelect.selectedIndex = selectedIndex;
+}
+
+export async function initVoices() {
+    voiceSelect = document.getElementById("voiceSelect");
+    populateVoiceList();
+
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+        speechSynthesis.onvoiceschanged = populateVoiceList;
+    }
+}
+
+export async function speak(inputText) {
+    if (synth.speaking) {
+        console.error("speechSynthesis.speaking");
+        return;
+    }
+
+    if (inputText !== "") {
+        const utterThis = new SpeechSynthesisUtterance(inputText);
+
+        utterThis.onend = function (event) {
+            console.log("SpeechSynthesisUtterance.onend");
+        };
+
+        utterThis.onerror = function (event) {
+            console.error("SpeechSynthesisUtterance.onerror");
+        };
+
+        const selectedOption =
+            voiceSelect.selectedOptions[0].getAttribute("data-name");
+
+        for (let i = 0; i < voices.length; i++) {
+            if (voices[i].name === selectedOption) {
+                utterThis.voice = voices[i];
+                break;
+            }
+        }
+        //utterThis.pitch = pitch.value;
+        //utterThis.rate = rate.value;
+        synth.speak(utterThis);
+    }
+}
+
+
+// references:
+// https://github.com/pavelsavara/blazor-wasm-hands-pose/blob/main/DetectHandsJsComponent/DetectHands.razor.js
+// https://peerjs.com/
+
+function insertGlobalScript(url) {
+    var element = document.createElement('script');
+    element.setAttribute('src', url);
+    element.setAttribute('crossorigin', 'anonymous');
+    document.head.appendChild(element);
+
+    return new Promise((resolve) => {
+        element.onload = () => {
+            resolve();
+        };
+    });
+}
+
+let dotnetExports;
+let hostConnection;
+let clientConnections = [];
+const hostDomain = "net-marckel-blazorbingo-";
+
+export async function host(component, id) {
+
+    const { getAssemblyExports } = await globalThis.getDotnetRuntime(0);
+    dotnetExports = await getAssemblyExports("BlazorBingo.dll");
+
+    await insertGlobalScript('https://unpkg.com/peerjs@1.4.7/dist/peerjs.min.js');
+
+    var peer = new Peer(hostDomain + id);
+
+    peer.on('open', function (id) {
+        console.log('My Peer Id is: ' + id);
+    });
+    
+    peer.on('connection', function (conn) {
+        clientConnections.push(conn);
+        var playerName = conn.metadata.playerName;
+        //console.log('received connection from ' + conn.peer + ' ' + playerName);
+        dotnetExports.BlazorBingo.Interop.OnDataReceived(component, "connected", playerName);
+
+        conn.on('data', function (data) {
+            //console.log(data);
+            dotnetExports.BlazorBingo.Interop.OnDataReceived(component, data.messageType, data.message);
+        });
+        conn.on('close', function () {
+            //console.log('connection to ' + playerName + ' has been closed.');
+            dotnetExports.BlazorBingo.Interop.OnDataReceived(component, "disconnected", playerName);
+        });
+    });
+
+    peer.on('error', function (err) {
+        console.log('error ' + err.type);
+    });
+}
+
+export async function connect(component, remoteId, playerName) {
+
+    const { getAssemblyExports } = await globalThis.getDotnetRuntime(0);
+    dotnetExports = await getAssemblyExports("BlazorBingo.dll");
+
+    await insertGlobalScript('https://unpkg.com/peerjs@1.4.7/dist/peerjs.min.js');
+
+    var peer = new Peer();
+
+    peer.on('open', function (id) {
+        console.log('My Peer Id is: ' + id);
+        console.log('Remote Id: ' + remoteId);
+        console.log('Player Name: ' + playerName);
+        hostConnection = peer.connect(hostDomain + remoteId, {
+            reliable: true,
+            metadata: {
+                playerName: playerName
+            }
+        });
+        hostConnection.on("open", () => {
+            console.log('connected');
+        });
+        hostConnection.on("data", (data) => {
+            //console.log(data);
+            dotnetExports.BlazorBingo.Interop.OnDataReceived(component, data.messageType, data.message);
+        });
+        hostConnection.on('close', function () {
+            console.log('connection to host has been closed.');
+        });
+    });
+
+    peer.on('error', function (err) {
+        console.log('error ' + err.type);
+    });
+}
+
+export async function notifyHost(messageType, message) {
+    //console.log(`notifyHost (${messageType}): ${message}`);
+    let data = {
+        messageType: messageType,
+        message: message,
+    };
+    hostConnection.send(data);
+}
+
+export async function broadcast(messageType, message) {
+    //console.log(`broadcast (${messageType}): ${message}`);
+    let data = {
+        messageType: messageType,
+        message: message,
+    };
+    clientConnections.forEach(function (conn) {
+        conn.send(data);
+    });
+}
+
+export async function shareUrl(title, text, url) {
+    if (navigator.share) {
+        navigator.share({
+            title: title,
+            text: text,
+            url: url,
+        })
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+    } else {
+        console.log('Share not supported on this browser, do it the old way.');
+    }
+}
---
+++ service-worker-assets.js
@@ -0,0 +1,245 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-gMMhXf930MHxSiJZcjP4DGT41x\/\/4UluuuaVRM2SZIk=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-2jNLve8W0sST0LAebgAc1IwSnGhkbjOKO8QPWWiJssI=",
+      "url": "_framework\/dotnet.7.0.0.4z8oi9khrl.js"
+    },
+    {
+      "hash": "sha256-NFPG+mWpX1lpilZGC5d9W\/8+Yjx+6oW2ujo2u+HYW1Q=",
+      "url": "_framework\/dotnet.timezones.blat"
+    },
+    {
+      "hash": "sha256-LI9mKk\/YPFYBcqG6w2XuRqIzLVo+C1xcOQQl8PmIFI0=",
+      "url": "_framework\/dotnet.wasm"
+    },
+    {
+      "hash": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk=",
+      "url": "_framework\/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-l7NtS3cI+3VUoQ2XQvlzMb8x4Ow1BtnOJNLWc6bifW0=",
+      "url": "_framework\/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-SNAStZO8ryN4gEbGzRYHea+b+VMpFMMNWXKTM3Y1gH4=",
+      "url": "_framework\/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-DXO3tJHXDOV4qgW\/w8ooggAgZdAVpa5ZllYYAQWpYIk=",
+      "url": "_framework\/icudt.dat"
+    },
+    {
+      "hash": "sha256-080WomZdk2INW8+QBAPVu723MbJq6btwM9QjTG7\/X+4=",
+      "url": "_framework\/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-FG5ZqS92BUEH9nCW6jv2ArTkNG7N\/bN1f0LoPdcxeGo=",
+      "url": "_framework\/BlazorBingo.dll"
+    },
+    {
+      "hash": "sha256-eXVgTw72kEn74FZgDQbLtvAmXbiZDSj5gzUnxVmwowQ=",
+      "url": "_framework\/Blazored.LocalStorage.dll"
+    },
+    {
+      "hash": "sha256-AZD3lsqA4VzPELFZYB59SjfmP2ukkLyH6JjO6iMgIzM=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.dll"
+    },
+    {
+      "hash": "sha256-KreRAGEKJQUGD335XeJUAis4Sr+4tkV6gXtJcd7FgKo=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Forms.dll"
+    },
+    {
+      "hash": "sha256-\/WLe9c+UqHRV02MeKJUjlGyKUOFpduH0kvuJaIwF7XE=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Web.dll"
+    },
+    {
+      "hash": "sha256-jj0gEWGz0IP5DtImmDS2JCnL7nDJvw39GXaOgt9q2BU=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-X\/f4fDl2cuIRXeWHhK\/f2UqQbFioD+RU4a4CEh0zrrQ=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-DBOKSPriP2JDxVbbWrLXyD3K4\/x3RBifNBWk\/q1I39M=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.dll"
+    },
+    {
+      "hash": "sha256-nTqLKuydttqLtE3VT3p6XhPmLxuaJDd0cL3Qzt8D4Ro=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Json.dll"
+    },
+    {
+      "hash": "sha256-7K40xNmwRvvPyoMmnYjiY9k\/cn+Zai4JPWd117WCQWI=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-qi0kE7rp0kdsNqdL6DyPZEeimjUGvcLT4iWQX0YnRus=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.dll"
+    },
+    {
+      "hash": "sha256-+2c3QhLfg+7\/\/BEZaCoObJ4Gk0SEanun0BNvaJADlzg=",
+      "url": "_framework\/Microsoft.Extensions.Logging.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-oEPPw1EPpaOHv+EHwoT2WcgDiRbjEXxigiW7V44cIYE=",
+      "url": "_framework\/Microsoft.Extensions.Logging.dll"
+    },
+    {
+      "hash": "sha256-+Lq6bvoSbvBJkKu5Ktwj749sv7wthRHrYX6JuFlIc\/E=",
+      "url": "_framework\/Microsoft.Extensions.Options.dll"
+    },
+    {
+      "hash": "sha256-eXvGx2jcjpTPEJoAHBsW\/VuMPbNyyU+AsuhPmkzSSRY=",
+      "url": "_framework\/Microsoft.Extensions.Primitives.dll"
+    },
+    {
+      "hash": "sha256-TuqJTb0eggqwRiEMluzCFQG8qnNScm5B5uNMrPMAnNw=",
+      "url": "_framework\/Microsoft.JSInterop.dll"
+    },
+    {
+      "hash": "sha256-bn\/jJGkSeaQbmqAl6uUM0y1joLrVEzldOc3y5mscYMk=",
+      "url": "_framework\/Microsoft.JSInterop.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-hhNvHjcneFfavbDzTVTr2ezJmxR\/MitVbt4KKCe3gvs=",
+      "url": "_framework\/System.Collections.Concurrent.dll"
+    },
+    {
+      "hash": "sha256-3XnQA40U0Saam\/ug5H7bHApUHqxkYO1t6dV7jB6xonc=",
+      "url": "_framework\/System.Collections.dll"
+    },
+    {
+      "hash": "sha256-UqhrvGCdOYxISKNRvDvd0vJD5kPeIsYBbjnrF0\/E76k=",
+      "url": "_framework\/System.Collections.NonGeneric.dll"
+    },
+    {
+      "hash": "sha256-GHhSqazFJgMqSn2Kyfgb9HGE3swhwyJotUVlLlGWJWk=",
+      "url": "_framework\/System.Collections.Specialized.dll"
+    },
+    {
+      "hash": "sha256-BLz3Wm0oM0Gjn8FyeXZ5AagxMamz+5pWuLYIoqAi6cI=",
+      "url": "_framework\/System.ComponentModel.dll"
+    },
+    {
+      "hash": "sha256-GiX2POpA\/krD2YEjbCDAxqR9fpKA0y7hAVWZPqHJ2vs=",
+      "url": "_framework\/System.ComponentModel.Primitives.dll"
+    },
+    {
+      "hash": "sha256-GfYjCKuFXw29zH8X6YGjB3iDewQL68kQA58U2N7\/ZAo=",
+      "url": "_framework\/System.ComponentModel.TypeConverter.dll"
+    },
+    {
+      "hash": "sha256-al1GzxyjyHL0BUoMB\/ZddlH\/bRnzfFqa74wGiekuYME=",
+      "url": "_framework\/System.Console.dll"
+    },
+    {
+      "hash": "sha256-LYfG4o44Ui5HAIHPAw+mESjlzm\/UY68pTKFWWhkaWIY=",
+      "url": "_framework\/System.dll"
+    },
+    {
+      "hash": "sha256-JJFsKrOp6vhM7kKVcwpT+Axxl0XverIwkfIRHitq7\/8=",
+      "url": "_framework\/System.Linq.dll"
+    },
+    {
+      "hash": "sha256-RldZ54\/oZH8DiTCvRaHB8EcIi9ub9D4BkiqHQLdfJIE=",
+      "url": "_framework\/System.Linq.Expressions.dll"
+    },
+    {
+      "hash": "sha256-lkiNjLkJ2em\/Yxu+h8wG3IOhLHlllT1CRSpFKfbz32U=",
+      "url": "_framework\/System.Memory.dll"
+    },
+    {
+      "hash": "sha256-+7xpPgtl+bi\/ectsaEWbBlMcQVoRUCwKN5V\/kENPnU0=",
+      "url": "_framework\/System.Net.Http.dll"
+    },
+    {
+      "hash": "sha256-MuTcqRHpR2asQLlGC2L0rULFl5QXctZmuM2nerWb9Uc=",
+      "url": "_framework\/System.Net.Primitives.dll"
+    },
+    {
+      "hash": "sha256-dF9kwRV\/KSD6fwOlHQW3FO4NQwrerApTLmf0Trk\/rU0=",
+      "url": "_framework\/System.ObjectModel.dll"
+    },
+    {
+      "hash": "sha256-9DgQqeNthTYgwcKHCeX\/I\/ssV4XucMRFSqYazcw+XY8=",
+      "url": "_framework\/System.Private.CoreLib.dll"
+    },
+    {
+      "hash": "sha256-4gw9wvLuLFlQI8dJNRapJj\/S5fxDYnpQgc\/V0xl71Tw=",
+      "url": "_framework\/System.Private.Uri.dll"
+    },
+    {
+      "hash": "sha256-uBnOhuAwYu3hRQbj0xBqfWCWou2vqClIDuH7dEyyiM4=",
+      "url": "_framework\/System.Runtime.dll"
+    },
+    {
+      "hash": "sha256-9EpzlKz7tC7tH75QyY0jiP7sTVxKaB2yxLr9zyO8xBY=",
+      "url": "_framework\/System.Runtime.InteropServices.JavaScript.dll"
+    },
+    {
+      "hash": "sha256-YGKI4Blu6uGKSa73sC6gf7n9LRzwUwHBcAjLigs8a9Q=",
+      "url": "_framework\/System.Security.Cryptography.dll"
+    },
+    {
+      "hash": "sha256-JS0HmSJDM\/QTLVaMTupTSrK1d\/us3hyL5hBgK4CUm4U=",
+      "url": "_framework\/System.Text.Encodings.Web.dll"
+    },
+    {
+      "hash": "sha256-LV9mwhMzYRvPRYx3NuPbAF5rPUCXM2kCEj6XQjh4Yso=",
+      "url": "_framework\/System.Text.Json.dll"
+    },
+    {
+      "hash": "sha256-5x3zoGsjh+VvEi0dFUUM9GwmrVj5s+bBY\/D0x7v8IkM=",
+      "url": "_framework\/System.Text.RegularExpressions.dll"
+    },
+    {
+      "hash": "sha256-UlxH1JQ36gPeBBB159hUnzi9gOi369dMtkWUApGgMyA=",
+      "url": "_framework\/System.Threading.dll"
+    },
+    {
+      "hash": "sha256-Pd8Rv+nydqgTgDJ0AGx8sLE3+HE2r7oAHYj61j710tg=",
+      "url": "BlazorBingo.styles.css"
+    },
+    {
+      "hash": "sha256-habWRdhyNWA3BSpThpS68DCuBwELhMh50wcQTBoMXWg=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-4mWsDy3aHl36ZbGt8zByK7Pvd4kRUoNgTYzRnwmPHwg=",
+      "url": "favicon.png"
+    },
+    {
+      "hash": "sha256-DbpQaq68ZSb5IoPosBErM1QWBfsbTxpJqhU0REi6wP4=",
+      "url": "icon-192.png"
+    },
+    {
+      "hash": "sha256-oEo6d+KqX5fjxTiZk\/w9NB3Mi0+ycS5yLwCKwr4IkbA=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-gg4wNxaL9UjJYD8aIKdklU131zV2Pxmw8cALogx5Avw=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-wLz3iY\/cO4e6vKZ4zRmo4+9XDpMcgKOvv\/zEU3OMlRo=",
+      "url": "lib\/bootstrap\/css\/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-\/3JMpzrFCpORhREpzUUcmOjDAVoo\/4Ufl9rSR7BSpXc=",
+      "url": "lib\/bootstrap\/css\/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-UJEjBXXR6D\/LuBZO9INkfGAxAQmCwZVF5+GntoW323Q=",
+      "url": "manifest.json"
+    },
+    {
+      "hash": "sha256-K12XbE5lMFUFH0OZVmPWJWD04O+lMXHgY1ajGyCopMw=",
+      "url": "script\/Interop.js"
+    }
+  ],
+  "version": "yueCIF3q"
+};
