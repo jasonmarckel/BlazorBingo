45059907a1c17e06e9b52419cf02de618013ae80
Jason Marckel <Jason.Marckel@lubrizol.com>
2023-01-15 22:25:37 -0500

    impose a 5 second delay between calls

--- BlazorBingo/GameSettings.cs
+++ BlazorBingo/GameSettings.cs
@@ -5,5 +5,5 @@ public class GameSettings
     public string PlayerName { get; set; } = string.Empty;
     public string Dauber { get; set; } = "random";
     public string CallerVoice { get; set; } = string.Empty;
-    public string CardTheme { get; set; } = "blue";
+    public string CardTheme { get; set; } = string.Empty;
 }
