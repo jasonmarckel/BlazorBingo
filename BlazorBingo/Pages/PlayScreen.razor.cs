f18b490c51985b8351ffc90accd2919d71f6b145
Jason Marckel <Jason.Marckel@lubrizol.com>
2023-01-17 00:10:34 -0500

    allow player to generate a new bingo card if the game has not started.

--- BlazorBingo/Pages/PlayScreen.razor.cs
+++ BlazorBingo/Pages/PlayScreen.razor.cs
@@ -12,10 +12,12 @@ public partial class PlayScreen : IMessageHandler
             case "pick":
                 Console.WriteLine($"pick: {data}");
                 flashboard!.Add(Convert.ToInt32(data.Substring(2)));
+                gameStarted = true;
                 if (!isMuted) { Interop.Speak(data.Replace("-", ", "), settings.CallerVoice, settings.SelectedLanguage); }
                 break;
             case "restart":
                 ClearCard();
+                gameStarted= false;
                 break;
         }
         this.StateHasChanged(); // force the UI to refresh
