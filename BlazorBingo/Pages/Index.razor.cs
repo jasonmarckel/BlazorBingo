4ea82c16b54079f11d9b8c043aee13eadbe4a751
Jason Marckel <Jason.Marckel@lubrizol.com>
2023-03-14 23:11:21 -0400

    moved C# code from inline on .razor files to code-behind .razor.cs files; removed hard-coded SDK version to use in global.json

---
+++ BlazorBingo/Pages/Index.razor.cs
@@ -0,0 +1,51 @@
+using System.Runtime.Versioning;
+
+namespace BlazorBingo.Pages;
+
+[SupportedOSPlatform("browser")]
+public partial class Index
+{
+    protected string gameCode = string.Empty;
+    protected string playerName = string.Empty;
+    protected bool canJoin;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await settings.LoadAsync();
+        playerName = settings.PlayerName;
+        if (string.IsNullOrEmpty(settings!.SelectedLanguage))
+        {
+            settings.SelectedLanguage = await Interop.GetLanguage();
+        }
+#if DEBUG
+        gameCode = "0000";
+        RefreshCanJoin();
+#endif
+        await Interop.ReleaseWakeLock();
+    }
+
+    private void RefreshCanJoin()
+    {
+        canJoin = !string.IsNullOrWhiteSpace(playerName) && !string.IsNullOrWhiteSpace(gameCode) && gameCode.Length == 4;
+    }
+
+    protected void OnGameCodeInput(string value)
+    {
+        gameCode = value;
+        RefreshCanJoin();
+    }
+
+    protected async Task Host()
+    {
+        settings.PlayerName = playerName;
+        await Interop.PrimeSpeechSynthesis(); // prime speechSynthesis for iOS
+        Navigation.NavigateTo("./host");
+    }
+
+    protected async Task Join()
+    {
+        settings.PlayerName = playerName;
+        await Interop.PrimeSpeechSynthesis(); // prime speechSynthesis for iOS
+        Navigation.NavigateTo($"./play/{gameCode.ToUpper()}");
+    }
+}
---
+++ BlazorBingo/Shared/Flashboard.razor.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorBingo.Shared;
+
+public partial class Flashboard
+{
+    [Parameter]
+    public int BoardSize { get; set; }
+
+    public bool IsFinishedCalling
+    {
+        get
+        {
+            return CalledNumbers.Count == BoardSize;
+        }
+    }
+
+    public List<int> CalledNumbers { get; private set; } = null !;
+    private int lastCalled;
+    private int z;
+    private const string BINGO = "BINGO";
+
+    protected override void OnInitialized()
+    {
+        z = BoardSize / 5;
+        CalledNumbers = new List<int>(capacity: BoardSize);
+        lastCalled = 0;
+    }
+
+    private string GetCssClass(int n)
+    {
+        if (lastCalled == n)
+        {
+            return "lastCalled";
+        }
+
+        return CalledNumbers.Contains(n) ? "visible" : "invisible";
+    }
+
+    public void ClearBoard()
+    {
+        CalledNumbers.Clear();
+        lastCalled = 0;
+        this.StateHasChanged(); // force the UI to refresh
+    }
+
+    public void UpdateCalledNumbersCSV(string value)
+    {
+        CalledNumbers.Clear();
+        CalledNumbers.AddRange(value.Split(",").Select(i => Int32.Parse(i)));
+        lastCalled = CalledNumbers.LastOrDefault();
+        this.StateHasChanged(); // force the UI to refresh
+    }
+
+    public string LastCalled
+    {
+        get
+        {
+            int col = ((lastCalled - 1) / z);
+            return $"{BINGO[col]}-{lastCalled}";
+        }
+    }
+
+    public string CalledNumbersCSV
+    {
+        get
+        {
+            return String.Join(",", CalledNumbers);
+        }
+    }
+
+    public void Pick()
+    {
+        int x;
+        do
+        {
+            x = ThreadSafeRandom.ThisThreadsRandom.Next(1, BoardSize + 1);
+        }
+        while (CalledNumbers.Contains(x));
+        CalledNumbers.Add(x);
+        lastCalled = x;
+        StateHasChanged(); // refresh the UI
+    }
+}
