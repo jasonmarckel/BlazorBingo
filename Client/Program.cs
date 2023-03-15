2a23b73d311c8a76a999b6218dfb07d6c1c322a8
Jason Marckel <Jason.Marckel@lubrizol.com>
2023-03-15 00:27:23 -0400

    renamed the client project and its containing folder

---
+++ Client/Program.cs
@@ -0,0 +1,21 @@
+using System.Runtime.InteropServices.JavaScript;
+using BlazorBingo;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<GameSettings>();
+
+#pragma warning disable CA1416 // Validate platform compatibility
+await JSHost.ImportAsync("interopModule", "../script/interop.js");
+#pragma warning restore CA1416 // Validate platform compatibility
+
+await builder.Build().RunAsync();
