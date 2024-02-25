fabaec0e3d1b53cbf4f04000eef3c569eed8618d
Jason Marckel <Jason.Marckel@lubrizol.com>
2024-02-25 17:25:35 -0500

    added try..catch to interop.js

--- Client/wwwroot/script/interop.js
+++ Client/wwwroot/script/interop.js
@@ -22,14 +22,18 @@ export async function isInstalled() {
 }
 
 export async function installPWA() {
-    // Show the install prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    const { outcome } = await deferredPrompt.userChoice;
-    // Optionally, send analytics event with outcome of user choice
-    console.log(`User response to the install prompt: ${outcome}`);
-    // We've used the prompt, and can't use it again, throw it away
-    deferredPrompt = null;
+    try {
+        // Show the install prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        const { outcome } = await deferredPrompt.userChoice;
+        // Optionally, send analytics event with outcome of user choice
+        console.log(`User response to the install prompt: ${outcome}`);
+        // We've used the prompt, and can't use it again, throw it away
+        deferredPrompt = null;
+    } catch (e) {
+        deferredPrompt = null;
+    }
 }
 
 export async function showModal(id) {
